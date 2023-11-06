fb574cc083266fe9a4263dab3303357d81e48217
Hale Arslan <75578501+fhalearslnn@users.noreply.github.com>
2023-11-06 15:29:58 +0300

    Update settings.py

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '', # database name in RDS is written here
-        'USER': '', # database master username in RDS is written here
-        'PASSWORD': config('PASSWORD'),
-        'HOST': '',  # database endpoint is written here
-        'PORT': '' # database port is written here
+        'NAME': 'halu', # database name in RDS is written here
+        'USER': 'admin', # database master username in RDS is written here
+        'PASSWORD': Deneme7575,
+        'HOST': 'dnm-rds.cd8fxgpbvttw.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'PORT': '3306' # database port is written here
     }
 }
 
